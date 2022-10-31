639a8566f825725775677825ca3290936adee914
kanangit <kananovich@gmail.com>
2022-10-31 15:58:42 -0400

    v. 004

--- backup/003b/libStepperK/src/StepperK.cpp
+++ backup/003b/libStepperK/src/StepperK.cpp
@@ -5,19 +5,16 @@
  * two-wire constructor.
  * Sets which wires should control the motor.
  */
-StepperK::StepperK(int number_of_steps, int pin_PU, int pin_DR,
-                   int pin_LS_CW, int pin_LS_CCW, int pin_MF)
+StepperK::StepperK(int number_of_steps, int pin_PU, int pin_DR, int pin_LS_CW, int pin_LS_CCW)
 {
   this->step_number = 0;                   // which step the motor is on
   this->direction = 0;                     // motor direction
   this->last_step_time = 0;                // timestamp in us of the last step taken
   this->number_of_steps = number_of_steps; // total number of steps for this motor
-  this ->steps_to_accelerate = 50; 
 
   // Arduino pins for the motor control connection:
   this->pin_PU = pin_PU;
   this->pin_DR = pin_DR;
-  this->pin_MF = pin_MF;
 
   // Arduino pins for limit swithches
   /* clocwise (when looking at the tip of the shaft of the motor)
@@ -28,68 +25,9 @@ StepperK::StepperK(int number_of_steps, int pin_PU, int pin_DR,
   // setup the pins on the microcontroller:
   pinMode(this->pin_PU, OUTPUT);
   pinMode(this->pin_DR, OUTPUT);
-  pinMode(this->pin_MF, OUTPUT);
   pinMode(pin_LS_CW, INPUT_PULLUP);
   pinMode(pin_LS_CCW, INPUT_PULLUP);
   pin_count = 2;
-}
-
-/*
- * Sets the number of steps motor takes to accelerate to the speed
- */
-void StepperK::setStepsToAccelerate(long no_steps)
-{
-  this->steps_to_accelerate = 60L * 1000L * 1000L / this->number_of_steps;
-}
-
-void StepperK::accel_and_jog(int steps_to_move)
-{
-  int steps_left = abs(steps_to_move); // how many steps to take
-  digitalWrite(pin_MF, HIGH);          // activate the motor
-
-  // determine direction based on whether steps_to_mode is + or -:
-  if (steps_to_move > 0)
-  {
-    this->direction = 1;
-  }
-  if (steps_to_move < 0)
-  {
-    this->direction = 0;
-  }
-
-  // decrement the number of steps, moving one step each time:
-  while (steps_left > 0)
-  {
-    unsigned long now = micros();
-    // move only if the appropriate delay has passed:
-    if (now - this->last_step_time >= this->step_delay)
-    {
-      // get the timeStamp of when you stepped:
-      this->last_step_time = now;
-      // increment or decrement the step number,
-      // depending on direction:
-      if (this->direction == 1)
-      {
-        this->step_number++;
-        if (this->step_number == this->number_of_steps)
-        {
-          this->step_number = 0;
-        }
-      }
-      else
-      {
-        if (this->step_number == 0)
-        {
-          this->step_number = this->number_of_steps;
-        }
-        this->step_number--;
-      }
-      // decrement the steps left:
-      steps_left--;
-      // tell the driver to make one step:
-      stepController(direction);
-    }
-  }
 }
 
 /*
@@ -107,7 +45,6 @@ void StepperK::setSpeed(long whatSpeed)
 void StepperK::step(int steps_to_move)
 {
   int steps_left = abs(steps_to_move); // how many steps to take
-  digitalWrite(pin_MF, HIGH);          // activate the motor
 
   // determine direction based on whether steps_to_mode is + or -:
   if (steps_to_move > 0)
--- backup/003b/shockit/StepperK.cpp
+++ backup/003b/shockit/StepperK.cpp
@@ -12,7 +12,6 @@ StepperK::StepperK(int number_of_steps, int pin_PU, int pin_DR,
   this->direction = 0;                     // motor direction
   this->last_step_time = 0;                // timestamp in us of the last step taken
   this->number_of_steps = number_of_steps; // total number of steps for this motor
-  this ->steps_to_accelerate = 50; 
 
   // Arduino pins for the motor control connection:
   this->pin_PU = pin_PU;
@@ -32,64 +31,6 @@ StepperK::StepperK(int number_of_steps, int pin_PU, int pin_DR,
   pinMode(pin_LS_CW, INPUT_PULLUP);
   pinMode(pin_LS_CCW, INPUT_PULLUP);
   pin_count = 2;
-}
-
-/*
- * Sets the number of steps motor takes to accelerate to the speed
- */
-void StepperK::setStepsToAccelerate(long no_steps)
-{
-  this->steps_to_accelerate = 60L * 1000L * 1000L / this->number_of_steps;
-}
-
-void StepperK::accel_and_jog(int steps_to_move)
-{
-  int steps_left = abs(steps_to_move); // how many steps to take
-  digitalWrite(pin_MF, HIGH);          // activate the motor
-
-  // determine direction based on whether steps_to_mode is + or -:
-  if (steps_to_move > 0)
-  {
-    this->direction = 1;
-  }
-  if (steps_to_move < 0)
-  {
-    this->direction = 0;
-  }
-
-  // decrement the number of steps, moving one step each time:
-  while (steps_left > 0)
-  {
-    unsigned long now = micros();
-    // move only if the appropriate delay has passed:
-    if (now - this->last_step_time >= this->step_delay)
-    {
-      // get the timeStamp of when you stepped:
-      this->last_step_time = now;
-      // increment or decrement the step number,
-      // depending on direction:
-      if (this->direction == 1)
-      {
-        this->step_number++;
-        if (this->step_number == this->number_of_steps)
-        {
-          this->step_number = 0;
-        }
-      }
-      else
-      {
-        if (this->step_number == 0)
-        {
-          this->step_number = this->number_of_steps;
-        }
-        this->step_number--;
-      }
-      // decrement the steps left:
-      steps_left--;
-      // tell the driver to make one step:
-      stepController(direction);
-    }
-  }
 }
 
 /*
