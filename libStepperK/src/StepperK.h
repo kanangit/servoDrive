9b730f6712f11ed1e9f218eb07380381f671ab16
kanangit <kananovich@gmail.com>
2022-11-04 10:27:45 -0400

    v. 017. Cleaned the code more

--- libStepperK/src/StepperK.h
+++ libStepperK/src/StepperK.h
@@ -1,13 +1,11 @@
 /*
+ * StepperK.h - stepper library for modern steppers with drivers
+ * with limit switches functionality added
+ * loosely based on
  * Stepper.h - Stepper library for Wiring/Arduino - Version 1.1.0
  *
- * Original library        (0.1)   by Tom Igoe.
- * Two-wire modifications  (0.2)   by Sebastian Gassner
- * Combination version     (0.3)   by Tom Igoe and David Mellis
- * Bug fix for four-wire   (0.4)   by Tom Igoe, bug fix from Noah Shibley
- * High-speed stepping mod         by Eugene Kozlenko
- * Timer rollover fix              by Eugene Kozlenko
- * Five phase five wire    (1.1.0) by Ryan Orendorff
+ * last changes log:
+ * 2022-11-04 by A. Kananovich
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -26,42 +24,59 @@
  */
 
 // ensure this library description is only included once
+#ifndef ARRMAXLENGTH
+#define ARRMAXLENGTH 1200 // maximum size of the double precision array Arduino Mega
+// can handle
+#endif
 #ifndef StepperK_h
 #define StepperK_h
 
 // library interface description
-class StepperK {
-  public:
-    // constructors:
-    StepperK(int number_of_steps, int pin_PU, int pin_DR, int pin_LS_CW, int pin_LS_CCW);
+class StepperK
+{
+public:
+  // constructors:
+  StepperK(int number_of_steps, int pin_PU, int pin_DR,
+           int pin_LS_CW, int pin_LS_CCW, int pin_MF, int steps_per_rev);
 
-    // speed setter method:
-    void setSpeed(long whatSpeed);
+  // speed setter method:
+  void setSpeed(long whatSpeed);
 
-    // mover method:
-    void step(int number_of_steps);
+  // method to set the number of steps the motor accelerates:
+  void setStepsToAccelerate(long finSpeed, long no_steps_acc);
 
-    int version(void);
+  // mover method:
+  void step(int number_of_steps);
 
-  private:
-   
-    void stepController(int dir); // in case an external controller takes 
-    //care of the step sequence
+  // mover method with acceleration:
+  void accel_and_jog(int steps_to_move);
 
-    int direction;            // Direction of rotation
-    unsigned long step_delay; // delay between steps, in ms, based on speed
-    int number_of_steps;      // total number of steps this motor can take
-    int step_number;          // which step the motor is on
-    int pin_count;
+  int version(void);
 
-    // motor pin numbers:
-    int pin_PU;
-    int pin_DR;
-    int pin_LS_CW;
-    int pin_LS_CCW;
+private:
+  void stepController(int dir); // in case an external controller takes
+  // care of the step sequence
 
-    unsigned long last_step_time; // timestamp in us of when the last step was taken
+  int direction;            // Direction of rotation
+  unsigned long step_delay; // delay between steps, in us, based on speed
+                            //(or terminal speed)
+  int number_of_steps;      // total number of steps this motor can take
+  int steps_per_rev;        // steps per revolution (should coinside )
+                            // with the external motor settings
+  int steps_to_accelerate;  // number of steps the motor accelerates.
+                            // should be < number_of_steps;
+  int step_number;          // which step the motor is on
+  int pin_count;
+  unsigned long arr_delays[ARRMAXLENGTH];
+
+  // motor pin numbers:
+  int pin_PU;
+  int pin_DR;
+  int pin_LS_CW;
+  int pin_LS_CCW;
+  int pin_MF;
+
+  unsigned long last_step_time; // timestamp in us of when the last step was taken
 };
 
 #endif
-
