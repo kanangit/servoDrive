d6f5236fb48c7143a97e7b180e2678939a1e1cb6
kanangit <kananovich@gmail.com>
2025-08-01 16:42:29 -0400

    refined the variable names

--- shockit/shockit.ino
+++ shockit/shockit.ino
@@ -23,7 +23,7 @@ const int stepsPerRevolution = 100; // change this to fit the number of steps pe
 
 // travel distance in steps. Positive number will move the translation stage forward,
 // negative - backward
-int steps_travel = stepsPerRevolution *2.5;
+int steps_travel = stepsPerRevolution *6;
 
 // speed in RPM: maximum limit is 3000
 int speed_rpm = 60;
@@ -31,7 +31,12 @@ int speed_rpm = 60;
 // number of steps motor accelerates to the speed: don't go below 5
 int steps_to_accel = 50; // max acceleration 100 step corresponds to one revolution.
 
-double acceleration2 = 10; // m/s^2 the acceleration of the moving piston as it hits the particles cloud
+int steps_rectilin = 50; // number of steps the piston moves with constant speed speed_rpm after the first aceeleration stage
+                         // (and before the second acceleration stage
+
+double acceleration2 = 3; // m/s^2 the acceleration of the moving piston as it hits the particles cloud
+
+int steps_to_accel_2 = 50; // number of steps the piston accelerates as it hits the particles 
 
 double lead = 0.010;         //the linear distance (in meters) the translation stage travels in one revolution of the screw
                             //(corresponding to one revolution of the motor shaft.
@@ -55,7 +60,7 @@ void setup()
 
   // set the speed at speed_rpm rpm, with steps_to_accel steps to accelerate:
   //myStepper.setStepsToAccelerate(speed_rpm, steps_to_accel);
-  myStepper.setStepsToAccelerateAgain(speed_rpm, steps_to_accel, 100, acceleration2 / lead / 2.0 / M_PI, 100);
+  myStepper.setStepsToAccelerateAgain(speed_rpm, steps_to_accel, steps_to_accel, acceleration2 / lead / 2.0 / M_PI, steps_to_accel_2);
 
 
 }
